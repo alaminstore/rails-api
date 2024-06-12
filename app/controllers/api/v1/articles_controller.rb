5be2d0c90a9841e0a11481c5e4a2207b64fafbfd
Sm Al-amin <alamin@Sms-MacBook-Air.local>
2024-06-12 13:51:21 +0600

    article api complete crud

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -2,12 +2,21 @@ class Api::V1::ArticlesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    render json: {
-      message: 'hello'
-    }
+    articles = Article.all 
+    render json: articles, status: 200
   end
 
   def show
+    article = Article.find_by(id: params[:id])
+    if article
+      render json: article, status: 200
+    else
+      render json: {
+        success: false,
+        code: 404,
+        message: "Article not found!",
+      }
+    end
   end
 
   def create
@@ -17,18 +26,44 @@ class Api::V1::ArticlesController < ApplicationController
       author: arti_params[:author]
     )
     if article.save
-      render json: article, status: 200
+      render json: article, status: 201
     else
       render json: {
+        success: false,
+        code: 404,
         error: "Error creationg new articles"
       }
     end
   end
 
   def update
+    article = Article.find_by(id: params[:id])
+    if article
+      article.update(title: params[:title], body: params[:body], author: params[:author])
+      render json: {
+         success: true, 
+         code: 204,
+         message: "Article updated successfully",
+        data: article
+      }
+    else
+      render json: {
+        success: false,
+        code: 404,
+        message: "Plese provide valid article"
+      }
+    end
   end
 
   def destroy
+    article = Article.find_by(id: params[:id])
+    if article
+      article.destroy
+      render json: {
+        success: true,
+        code: 204
+        message:
+      }
   end
 
   private
